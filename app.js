c92ac09bce87dc99567fdf2ce36c8051181a8059
Simone823 <simone.daglio823@outlook.it>
2022-02-04 16:36:40 +0100

    userAge e if is nan

--- app.js
+++ app.js
@@ -8,5 +8,17 @@ if (isNaN(kmUtente)) {
     alert ("Inserisci un numero");
     let kmUtente = parseInt (prompt("Quanti chilometri vuoi percorrere? (Digita qui il numero di km)" ));
 }
-console.log(kmUtente)
+console.log(kmUtente);
+
+
+// Chiedo all'utente quanti anni ha
+let userAge = parseInt (prompt("Quanti anni hai? (Digita qui"));  
+console.log(userAge);
+
+// Se l'utente non digita un numero (quindi NaN), ripeto nuovamente riga 15
+if (isNaN(userAge)){
+    alert("Digita un numero");
+    let userAge = parseInt (prompt("Quanti anni hai? (Digita qui)"));
+}
+console.log(userAge);
 
