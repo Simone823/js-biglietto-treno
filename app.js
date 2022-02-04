168346b793d1f9886b359f348343445f4b8efc29
Simone823 <simone.daglio823@outlook.it>
2022-02-04 17:15:32 +0100

    modifica if is nan kmUtente, userAge

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ console.log(kmUtente);
 // Se l'utente non digita un numero (quindi NaN), ripeto nuovamente riga 3
 if (isNaN(kmUtente)) {
     alert ("Inserisci un numero");
-    let kmUtente = parseInt (prompt("Quanti chilometri vuoi percorrere? (Digita qui il numero di km)" ));
+    kmUtente = parseInt (prompt("Quanti chilometri vuoi percorrere? (Digita qui il numero di km)" ));
 }
 console.log(kmUtente);
 
@@ -18,7 +18,7 @@ console.log(userAge);
 // Se l'utente non digita un numero (quindi NaN), ripeto nuovamente riga 15
 if (isNaN(userAge)){
     alert("Digita un numero");
-    let userAge = parseInt (prompt("Quanti anni hai? (Digita qui)"));
+    userAge = parseInt (prompt("Quanti anni hai? (Digita qui)"));
 }
 console.log(userAge);
 
